2639eca753d673c4ebdfea88ac83d8a19ec9abc2
ridha <ridhaos.commerce@gmail.com>
2026-05-22 23:50:51 +0200

    Circular buffer put in place

--- aesd-char-driver/aesd-circular-buffer.c
+++ aesd-char-driver/aesd-circular-buffer.c
@@ -29,9 +29,36 @@
 struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct aesd_circular_buffer *buffer,
             size_t char_offset, size_t *entry_offset_byte_rtn )
 {
-    /**
-    * TODO: implement per description
-    */
+
+    uint8_t tail = buffer->in_offs;
+    uint8_t head = buffer->out_offs;
+    uint8_t bufferSize = 0;
+
+    if ((tail == head) && buffer->full)
+    {
+        bufferSize = AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
+    }else if (tail != head)
+    {
+        bufferSize = (tail - head + AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED) % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
+    }else
+    {
+        /* Buffer is empty */
+        return NULL;
+    }
+    size_t count = 0;
+    for(uint8_t i=0; i<bufferSize; i++)
+    {
+        uint8_t current = (head + i) % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
+        if ((char_offset >= count) &&
+            (char_offset < (count + buffer->entry[current].size)))
+        {
+            *entry_offset_byte_rtn = char_offset-count;
+            return &buffer->entry[current];
+        }
+        
+        count += buffer->entry[current].size;
+    }
+    
     return NULL;
 }
 
@@ -44,9 +71,24 @@ struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct
 */
 void aesd_circular_buffer_add_entry(struct aesd_circular_buffer *buffer, const struct aesd_buffer_entry *add_entry)
 {
-    /**
-    * TODO: implement per description
-    */
+    uint8_t tail = buffer->in_offs;
+    uint8_t next = (tail + 1) % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED; 
+    buffer->entry[tail] = *add_entry;
+
+    if (buffer->full)
+    {   
+        buffer->in_offs = next;
+        buffer->out_offs = next;
+    }else{
+        buffer->in_offs = next;
+    }
+
+    if (buffer->in_offs == buffer->out_offs)
+    {
+        buffer->full = true;
+    }else{
+        buffer->full = false;
+    }
 }
 
 /**
